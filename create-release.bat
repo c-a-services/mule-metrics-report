d9b94a43ae521c6ff7f7c88ae2f265bfebbd898e
Stefan CORDES <dt0b35@canda.com>
2018-03-07 16:43:24 +0100

    create-release.bat

---
+++ create-release.bat
@@ -0,0 +1,4 @@
+echo on
+
+call mvn build-helper:parse-version release:prepare release:perform -DdevelopmentVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT -DreleaseVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} -Dtag=mule-metrics-report-${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} -DpreparationGoals=clean -Darguments="-Dmaven.test.skip=true"
+
